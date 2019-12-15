fea8650c6787339412d913ceac2602a3fae12923
null-python <lybica1780@gmail.com>
2019-12-15 17:01:46 -0500

    Test 2

--- TempProj/src/main.java
+++ TempProj/src/main.java
@@ -6,6 +6,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("hi");
 		System.out.println("1 + 1 = " + 2);
 	}
 }
