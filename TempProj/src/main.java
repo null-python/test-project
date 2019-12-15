613912e930f0b81dcc86b8d322fb6010fa9d837a
null-python <lybica1780@gmail.com>
2019-12-15 16:59:48 -0500

    Test commit

--- TempProj/src/main.java
+++ TempProj/src/main.java
@@ -5,7 +5,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");
+		System.out.println("hello");
 		System.out.println("1 + 1 = " + 2);
 	}
 }
