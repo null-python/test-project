915279d01bdbe6219595a5428e26379bf4702c76
null-python <lybica1780@gmail.com>
2019-12-15 16:57:27 -0500

    Test commit

---
+++ TempProj/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("1 + 1 = " + 2);
+	}
+}
